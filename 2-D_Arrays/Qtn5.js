4aa3fc298b8320a10ec5a4f8035daa05d926afff
MIDUNRAJL <135823975+MIDUNRAJL@users.noreply.github.com>
2023-07-18 00:45:41 +0530

    completed

--- 2-D_Arrays/Qtn5.js
+++ 2-D_Arrays/Qtn5.js
@@ -1,10 +1,10 @@
 let arr = ["assignment", "problem", "media", "upload"]
+let empty_arr = [];
 for(let i=0; i<arr.length;i++){
-    if(arr[i][0]=='a' || arr[i[arr[i].length]]=='a')
+    if(arr[i][0]=='a' || arr[i][arr[i].length-1]=='a')
     {
-        console.log(arr)
+        empty_arr.push(arr[i]);
       
-    }
-
-    
+    }    
 }
+console.log(empty_arr)
