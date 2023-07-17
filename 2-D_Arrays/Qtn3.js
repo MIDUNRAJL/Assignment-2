f210dd5df0a6413f7ca5ac45c53d790bcfc37ae5
MIDUNRAJL <135823975+MIDUNRAJL@users.noreply.github.com>
2023-07-17 16:23:26 +0530

    completed

---
+++ 2-D_Arrays/Qtn3.js
@@ -0,0 +1,12 @@
+let arr = [[1,2,3],[4,5,6],[7,8,9],[7,8,9],[7,8,9]]
+// for(let i =0;i<arr.length;i++){
+//     // for(let j=0; j<arr.length;j++)
+//     // {
+      
+      
+//     // }
+// console.log(arr.length)
+// co
+// }
+console.log(arr.length)
+console.log(arr[3].length)
